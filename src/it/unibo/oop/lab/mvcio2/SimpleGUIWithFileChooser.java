f990e9fb1642d4de324f1257374fccf1e6d743ad
Enrico <errotagliaferri.4801@gmail.com>
2021-11-18 21:53:25 +0100

    Copy GUI from it.unibo.oop.lab.mvcio.SimpleGUI and add browse button and textfield

--- src/it/unibo/oop/lab/mvcio2/SimpleGUIWithFileChooser.java
+++ src/it/unibo/oop/lab/mvcio2/SimpleGUIWithFileChooser.java
@@ -1,5 +1,21 @@
 package it.unibo.oop.lab.mvcio2;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import it.unibo.oop.lab.mvcio.Controller;
+
 /**
  * A very simple program using a graphical interface.
  * 
@@ -31,5 +47,51 @@ public final class SimpleGUIWithFileChooser {
      * update the UI: in this example the UI knows when should be updated, so
      * try to keep things separated.
      */
+    
+    private final JFrame frame = new JFrame();
+
+    public SimpleGUIWithFileChooser() {
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / 2, sh / 2);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("SimpoleGUIWithFileChooser");
+        final JPanel canvas = new JPanel();
+        canvas.setLayout(new BorderLayout());
+        final JTextField browseTextField = new JTextField();
+        final JButton browse = new JButton("Browse");
+        browse.setLayout(new BorderLayout());
+        canvas.add(browseTextField);
+        canvas.add(browse, BorderLayout.EAST);
+        final JTextArea textArea = new JTextArea();
+        final JButton save = new JButton("Save");
+        save.setLayout(new BorderLayout());
+        frame.add(canvas, BorderLayout.NORTH);
+        frame.add(textArea);
+        frame.add(save, BorderLayout.SOUTH);
+        /*
+         * Handlers
+         */
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                final Controller controller = new Controller();
+                controller.setCurrentFile("output.txt");
+                try {
+                    controller.write(textArea.getText());
+                } catch (IOException e1) {
+                    JOptionPane.showMessageDialog(frame, e1, "Error", JOptionPane.ERROR_MESSAGE);
+                    e1.printStackTrace();
+                }
+            }
+        });
+        frame.setLocationByPlatform(true);
+        frame.setVisible(true);
+    }
+    
+    public static void main(final String[] args) {
+        new SimpleGUIWithFileChooser();
+    }
 
 }
