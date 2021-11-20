c5a9413f85b28b33b2fd2bf6f6d87086a8a99a16
Enrico <errotagliaferri.4801@gmail.com>
2021-11-20 14:48:51 +0100

    Implement controller interface

---
+++ src/it/unibo/oop/lab/mvc/ControllerImpl.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package it.unibo.oop.lab.mvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ControllerImpl implements Controller {
+    
+    private String currentString;
+    private List<String> history;
+    
+    public ControllerImpl() {
+        this.history = new ArrayList<>();
+    }
+
+    @Override
+    public void setString(String str) {
+        this.currentString = str;
+        this.history.add(str);
+    }
+
+    @Override
+    public String getString() {
+        return this.currentString;
+    }
+
+    @Override
+    public List<String> getHistory() {
+        return this.history;
+    }
+
+    @Override
+    public void printString() throws IllegalStateException {
+        if (this.getString() == null) {
+            throw new IllegalStateException();
+        }
+        System.out.println(this.getString());
+    }
+
+}
