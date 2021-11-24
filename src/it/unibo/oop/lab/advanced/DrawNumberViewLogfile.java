eb9ffdbf2067de3b4f05330e9a8b29fecfb81c6a
Enrico <errotagliaferri.4801@gmail.com>
2021-11-24 22:15:41 +0100

    Implement logfile class

--- src/it/unibo/oop/lab/advanced/DrawNumberViewLogfile.java
+++ src/it/unibo/oop/lab/advanced/DrawNumberViewLogfile.java
@@ -1,36 +1,49 @@
 package it.unibo.oop.lab.advanced;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
 public class DrawNumberViewLogfile implements DrawNumberView {
+
+    private DrawNumberViewObserver observer;
+    PrintStream out;
+    
+    public DrawNumberViewLogfile(final String path) throws IOException {
+        this.out = new PrintStream(new FileOutputStream(new File(path)));
+    }
+
+    public DrawNumberViewLogfile(final PrintStream stream) {
+        this.out = stream;
+    }
+
+
 
     @Override
     public void setObserver(DrawNumberViewObserver observer) {
-        // TODO Auto-generated method stub
-
+        this.observer = observer;
     }
 
     @Override
     public void start() {
-        // TODO Auto-generated method stub
 
     }
 
     @Override
     public void numberIncorrect() {
-        // TODO Auto-generated method stub
-
+        this.out.println("Number is incorrect... Try again");
     }
 
     @Override
-    public void result(DrawResult res) {
-        // TODO Auto-generated method stub
-
+    public void result(final DrawResult res) {
+        this.out.println(res);
     }
 
 
     @Override
-    public void displayError(String message) {
-        // TODO Auto-generated method stub
-
+    public void displayError(final String message) {
+        this.out.println(message);
     }
 
 }
