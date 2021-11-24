f916b65eb028b179fce5b41139c62827685a3f5b
Enrico <errotagliaferri.4801@gmail.com>
2021-11-24 21:17:44 +0100

    Create logfile view class

---
+++ src/it/unibo/oop/lab/advanced/DrawNumberViewLogfile.java
@@ -0,0 +1,41 @@
+package it.unibo.oop.lab.advanced;
+
+public class DrawNumberViewLogfile implements DrawNumberView {
+
+    @Override
+    public void setObserver(DrawNumberViewObserver observer) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void start() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void numberIncorrect() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void result(DrawResult res) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void limitsReached() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void displayError(String message) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
